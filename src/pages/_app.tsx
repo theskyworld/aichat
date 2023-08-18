edc1bad2dd8d6d265f89a8e54d95e03c25b6382e
theskyworld <2456450687@qq.com>
2023-08-19 00:36:44 +0800

    * init aichat project with NextJS * revise colorScheme, "dark" & "light" * add Notification

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,35 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { useState } from "react";
+import {
+  MantineProvider,
+  ColorSchemeProvider,
+  ColorScheme,
+} from "@mantine/core";
+import { Notifications } from "@mantine/notifications";
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [colorScheme, setColorScheme] = useState<ColorScheme>("light");
+  // 切换页面的dark和light颜色主题
+  const toggleColorScheme = (value?: ColorScheme) => {
+    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+  };
+  // 使用mantine样式
+  // 页面颜色主题默认为dark
+  return (
+    <ColorSchemeProvider
+      colorScheme="dark"
+      toggleColorScheme={toggleColorScheme}
+    >
+      {/* 提供全局配置属性 */}
+      <MantineProvider
+        theme={{ colorScheme, primaryColor: "green" }}
+        withNormalizeCSS
+        withGlobalStyles
+      >
+        {/* 配置全局的Notification组件 */}
+        <Notifications position="top-right" zIndex={2077}></Notifications>
+        <Component {...pageProps} />
+      </MantineProvider>
+    </ColorSchemeProvider>
+  );
 }
